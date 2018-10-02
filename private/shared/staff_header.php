db3cfda7f1a488053c90c8448a761e19d15355ee
AlexTicovschi <alexmticovschi@gmail.com>
2018-10-02 16:41:40 +0100

    using URL parameters

--- private/shared/staff_header.php
+++ private/shared/staff_header.php
@@ -9,7 +9,7 @@
     <title>GBI - <?php echo $page_title; ?></title>
     <meta charset="utf-8">
 
-	<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo url_for('/stylesheets/staff.css'); ?>">
   </head>
 
--- public/staff/subjects/index.php
+++ public/staff/subjects/index.php
@@ -1,10 +1,57 @@
 <?php require_once('../../../private/initialize.php'); ?>
 
+<?php
+  $subjects = [
+    ['id' => '1', 'position' => '1', 'visible' => '1', 'menu_name' => 'About Globe Bank'],
+    ['id' => '2', 'position' => '2', 'visible' => '1', 'menu_name' => 'Consumer'],
+    ['id' => '3', 'position' => '3', 'visible' => '1', 'menu_name' => 'Small Business'],
+    ['id' => '4', 'position' => '4', 'visible' => '1', 'menu_name' => 'Commercial'],
+  ];
+?>
+
+
 <?php $page_title = 'Subjects'; ?>
 <?php include(SHARED_PATH . '/staff_header.php') ?>
 
 	<div id="content">
-		
+		<div class="subjects listing">
+			<h1>Subjects</h1>
+
+			<div class="actions">
+				<a class="action" href="">Create New Subject</a>
+			</div>
+
+
+			<table class="table list">
+			  <thead>
+			    <tr>
+			      <th scope="col">ID</th>
+			      <th scope="col">Position</th>
+			      <th scope="col">Visible</th>
+			      <th scope="col">Name</th>
+			      <th scope="col">&nbsp;</th>
+			      <th scope="col">&nbsp;</th>
+			      <th scope="col">&nbsp;</th>
+			    </tr>
+			  </thead>
+			  <tbody>
+
+			  	<?php foreach($subjects as $subject) { ?>
+			  			<tr>
+					      <td><?php echo $subject['id']; ?></td>
+					      <td><?php echo $subject['position']; ?></td>
+					      <td><?php echo $subject['visible'] == 1 ? 'true' : 'false'; ?></td>
+					      <td><?php echo $subject['menu_name']; ?></td>
+					      <td><a href="<?php echo url_for('/staff/subjects/show.php?id=' . $subject['id']); ?>" class="action">View</a></td>
+					      <td><a href="" class="action">Edit</a></td>
+					      <td><a href="" class="action">Delete</a></td>
+					    </tr>
+			  	<?php	} ?>
+			    
+			  </tbody>
+			</table>
+
+		</div>
 	</div>
 
 <?php include(SHARED_PATH . '/staff_footer.php') ?>
