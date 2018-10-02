9253f23dce68e34bd0b9bb7164740e0d6db107b7
AlexTicovschi <alexmticovschi@gmail.com>
2018-10-02 15:04:50 +0100

    including files

---
+++ private/shared/staff_header.php
@@ -0,0 +1,22 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>GBI</title>
+    <meta charset="utf-8">
+
+	<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> -->
+    <link rel="stylesheet" href="../stylesheets/staff.css">
+  </head>
+
+  <body class="Site">
+
+	<header>
+		<h1>GBI Staff Area</h1>
+	</header>
+
+	<nav>
+		<ul>
+			<li><a href="index.php">Menu</a></li>
+		</ul>
+	</nav>
--- public/staff/index.php
+++ public/staff/index.php
@@ -1,33 +1,10 @@
-<!doctype html>
+<?php require_once('../../private/initialize.php'); ?>
+<?php include('../../private/shared/staff_header.php') ?>
 
-<html lang="en">
-  <head>
-    <title>GBI</title>
-    <meta charset="utf-8">
-    
-	<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> -->
-    <link rel="stylesheet" href="../stylesheets/staff.css">
-  </head>
-
-  <body class="Site">
-
-	<header>
-		<h1>GBI Staff Area</h1>
-	</header>
-
-	<nav>
-		<ul>
-			<li><a href="index.php">Menu</a></li>
-		</ul>
-	</nav>
 
 	<div id="content">
 		
 	</div>
 
-	<footer>
-		&copy; <?php echo date('Y'); ?> Globe Bank
-	</footer>
-  </body>
-</html>
+<?php include('../../private/shared/staff_footer.php') ?>
 
