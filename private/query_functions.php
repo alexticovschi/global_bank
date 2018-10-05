83375a9d0602b9409638c7025278e82cad6b96b8
AlexTicovschi <alexmticovschi@gmail.com>
2018-10-05 18:59:28 +0100

    handling errors

--- private/query_functions.php
+++ private/query_functions.php
@@ -2,10 +2,12 @@
 
 function find_all_subjects() {
 	global $db;
-	
+
 	$query  = "SELECT * FROM subjects ";
 	$query .= "ORDER BY position ASC";
+	// echo $query;
 	$result = mysqli_query($db, $query);
+	confirm_result($result);
 	return $result;
 }
 
